ac685fabae070a1080b19a69a5c8d790c55650d9
lWaterLite <lwaterlite1@outlook.com>
2025-10-25 23:33:45 +0800

    chore(debug): add script to copy dll to mods folder.

---
+++ run.ps1
@@ -0,0 +1,15 @@
+﻿$SourceFile = "C:\work\Program_space\Rider\ItemPickHint\ItemPickHint\bin\Debug\netstandard2.1\ItemPickHint.dll"
+$DestinationPath = "D:\SteamLibrary\steamapps\common\Escape from Duckov\Duckov_Data\Mods\ItemPickHint\"
+
+if (-not (Test-Path $SourceFile)) {
+    Write-Error "源文件 '$SourceFile' 不存在！"
+    exit 1
+}
+
+try {
+    Copy-Item -Path $SourceFile -Destination $DestinationPath -Force
+    Write-Host "Copy Complete."
+} catch {
+    Write-Error "文件复制失败: $_"
+    exit 1
+}
