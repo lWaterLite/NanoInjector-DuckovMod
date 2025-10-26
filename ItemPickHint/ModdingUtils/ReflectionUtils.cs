b5192613e445172ec88afce6f4093b6860399240
lWaterLite <lwaterlite1@outlook.com>
2025-10-26 13:34:01 +0800

    refactor(ModdingUtils): refactor ModdingUtils, including ItemUtils and ReflectionUtils

---
+++ ItemPickHint/ModdingUtils/ReflectionUtils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+
+namespace ItemPickHint.ModdingUtils
+{
+    public static class ReflectionUtils
+    {
+        public static object SetPrivateField(this object target, string fieldName, object value)
+        {
+            FieldInfo? fieldInfo = target.GetType().GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic);
+            if (fieldInfo == null)
+            {
+                Debug.LogWarning($"VEI: Cannot find field {fieldName} in object {target}, fail to set value.");
+                return target;
+            }
+            fieldInfo.SetValue(target, value);
+            return target;
+        }
+
+        public static object? GetPrivateField(object target, string fieldName)
+        {
+            FieldInfo? info = target.GetType().GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic);
+            if (info != null) return info.GetValue(target);
+            Debug.LogWarning($"VEI: Cannot find field {fieldName} in object {target}, fail to get value.");
+            return null;
+        }
+    }
+}
