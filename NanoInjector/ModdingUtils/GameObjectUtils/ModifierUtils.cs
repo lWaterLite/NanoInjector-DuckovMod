563a16ff671cab8666cb33a9db45f2e84cfc8c7a
lWaterLite <lwaterlite1@outlook.com>
2025-10-28 22:09:42 +0800

    feat(ModifierUtils): Create ModifierUtils for modifier injection

---
+++ NanoInjector/ModdingUtils/GameObjectUtils/ModifierUtils.cs
@@ -0,0 +1,38 @@
+﻿using ItemStatsSystem.Stats;
+
+namespace NanoInjector.ModdingUtils.GameObjectUtils
+{
+    public static class ModifierUtils
+    {
+        
+    }
+
+    public class ConfigModifierAction : ModifierAction
+    {
+        private bool _isConfig;
+        
+        protected override void Awake()
+        {
+            if (!_isConfig) return;
+            base.Awake();
+        }
+
+        public void SetConfig(ModifierInfo info)
+        {
+            _isConfig = true;
+            enabled = true;
+            targetStatKey = info.StatKey;
+            ModifierType = info.ModifierType;
+            modifierValue = info.Value;
+            
+            base.Awake();
+        }
+    }
+
+    public class ModifierInfo
+    {
+        public ModifierType ModifierType;
+        public string StatKey = "default";
+        public float Value;
+    }
+}
