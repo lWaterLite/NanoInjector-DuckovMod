0b7ccea7d665a45d1db6224c1aa599ce7d4f4280
lWaterLite <lwaterlite1@outlook.com>
2025-10-29 15:19:47 +0800

    chore(run.ps1): Add languages support

--- run.ps1
+++ run.ps1
@@ -1,15 +1,7 @@
-﻿$SourceFile = "C:\work\Program_space\Rider\NanoInjector\NanoInjector\bin\Debug\netstandard2.1\NanoInjector.dll"
+﻿$SourceFile = "C:\work\Program_space\Rider\NanoInjector\NanoInjector\bin\Debug\netstandard2.1\"
 $DestinationPath = "D:\SteamLibrary\steamapps\common\Escape from Duckov\Duckov_Data\Mods\ItemPickHint\"
 
-if (-not (Test-Path $SourceFile)) {
-    Write-Error "源文件 '$SourceFile' 不存在！"
-    exit 1
-}
+Copy-Item -Path "$SourceFile\NanoInjector.dll" -Destination $DestinationPath -Force
+Copy-Item -Path "$SourceFile\Languages\*" -Destination "$DestinationPath\Languages" -Recurse -Force
 
-try {
-    Copy-Item -Path $SourceFile -Destination $DestinationPath -Force
-    Write-Host "Copy Complete."
-} catch {
-    Write-Error "文件复制失败: $_"
-    exit 1
-}
+Write-Host "After-build Copy Complete."
